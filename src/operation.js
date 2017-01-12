78e9f421afc6c4311fc51f04cbfc7eb50a9707e2
rattlesnake <ricky.ratnayake@gmail.com>
2017-01-13 10:01:44 +1100

    added resolve and reject

--- src/operation.js
+++ src/operation.js
@@ -1,5 +1,8 @@
-
 const delayms = 1;
+const expectedCurrentCity = "New York, NY";
+const expectedForecast = {
+  fiveDay: [60, 70, 80, 45, 50]
+};
 
 function getCurrentCity(callback) {
   setTimeout(function () {
@@ -35,11 +38,7 @@ function getForecast(city, callback) {
       return;
     }
 
-    const fiveDay = {
-      fiveDay: [60, 70, 80, 45, 50]
-    };
-
-    callback(null, fiveDay)
+    callback(null, expectedForecast);
 
   }, delayms)
 }
@@ -51,7 +50,7 @@ function fetchForecast(city){
     if (err){
       operations.fail(err);
     } else {
-      operations.succeed(res);
+      operations.resolve(res);
     }
   });
 
@@ -66,7 +65,7 @@ function fetchWeather(city){
       operations.fail(err);
       return;
     }
-    operations.succeed(res);
+    operations.resolve(res);
   });
 
   return operations;
@@ -74,19 +73,47 @@ function fetchWeather(city){
 
 function fetchCurrentCity(){
   const operations = new Operation();
-  
+
   getCurrentCity((err, res) => {
     console.log(res);
     if (err){
       operations.fail(err);
       return;
     }
-    operations.succeed(res);
+    operations.resolve(res);
   });
 
   return operations;
 }
 
+const cityFailError = new Error('Gps Broken.');
+function fetchCurrentCityThatFails(){
+  let op = new Operation();
+
+  waitAsync(() => {
+    op.fail(cityFailError);
+  });
+
+  return op;
+}
+
+function fetchCurrentCityIndecisive(){
+  const op = new Operation();
+  waitAsync(() => {
+    op.resolve('nyc');
+    op.resolve('philly');
+  });
+  return op;
+}
+
+function fetchCurrentCityRepeatFail(){
+  const op = new Operation();
+  waitAsync(() => {
+    op.fail(new Error('first fail'));
+    op.fail(new Error('second fail'));
+  });
+  return op;
+}
 
 function Operation(){
   const operation = {
@@ -94,11 +121,28 @@ function Operation(){
     _errorReactions: [],
     _state: 'pending',
     _result: null,
+    _isComplete: () => {
+      return operation._state === 'success' ||
+              operation._state === 'fail';
+    },
+    then: onCompletion,
+    catch: onFailure,
+    resolve: resolve,
+    reject: fail,
+    onCompletion,
+    onFailure,
+    fail,
+    _succeed,
+    clear,
   };
 
-  operation.onCompletion = function onCompletion(successFn, errorFn){
-    const completionOp = new Operation();
-    
+
+  return operation;
+
+  ////Class functions//////
+  function onCompletion(successFn, errorFn){
+    const proxyOp = new Operation();
+
     if (operation._state === 'fail'){
       return errorHandler();
     }
@@ -109,63 +153,210 @@ function Operation(){
     operation._successReactions.push(successHandler);
     operation._errorReactions.push(errorHandler);
 
-    return completionOp;
+    return proxyOp;
 
     /// Private Functions ////
     function successHandler(){
-      if (successFn) {
-        let callbackRes = successFn(operation._result);
-        if (callbackRes && callbackRes.onCompletion){
-          return callbackRes.forwardCompletion(completionOp);
+      return waitAsync(() => {
+        let callbackRes;
+        if (successFn) {
+          try{
+            callbackRes = successFn(operation._result);
+          } catch (successFnErr){
+            return proxyOp.fail(successFnErr);
+          }
+          return proxyOp.resolve(callbackRes);
         }
-      }
+        return proxyOp.resolve(operation._result);
+      });
     }
 
     function errorHandler(){
-      if (errorFn){
-        errorFn(operation._result);
-      }
+      return waitAsync(() => {
+        let callbackRes;
+        if (errorFn){
+          try {
+            callbackRes = errorFn(operation._result);
+          } catch (errorFnErr){
+            return proxyOp.fail(errorFnErr);
+          }
+          return proxyOp.resolve(callbackRes);
+        }
+        return proxyOp.fail(operation._result);
+      });
     }
   };
-  operation.then = operation.onCompletion;
 
-  operation.onFailure = function onFailure(error){
+  function resolve(value){
+    if (operation._isComplete()){
+      return;
+    }
+
+    if (value && value.then){
+      return value.then(operation.resolve, operation.fail);
+    }
+    return operation._succeed(value);
+  };
+
+  function onFailure(error){
     return operation.onCompletion(null, error);
   };
 
-  operation.fail = function fail(err){
+  function fail(err){
+    if (operation._isComplete()){
+      return;
+    }
     operation._state = 'fail';
     operation._result = err;
 
-    operation._errorReactions.forEach((fn) => {
+    operation._errorReactions
+      .forEach((fn) => {
         fn(err);
-    });
+      });
     operation.clear();
   };
 
-  operation.succeed = function succeed(result){
+  function _succeed(result){
     operation._state = 'success';
     operation._result = result;
-    operation._successReactions.forEach((fn) => {
-      fn(result);
-    });
+    operation._successReactions
+      .forEach((fn) => {
+        fn(result);
+      });
     operation.clear();
   };
 
-  operation.clear = function clear(){
+  function clear(){
     operation._successReactions = [];
     operation._errorReactions = [];
   };
-  
-  operation.forwardCompletion = function forwardCompletion(op){
-    operation.onCompletion(op.succeed, op.fail);
-  };
-
-  return operation;
 }
 
-
 suite('operations');
+
+test('what is resolve?', done => {
+  const fc = new Operation();
+  fc.resolve('nyc');
+
+  fclone = new Operation();
+  fclone.resolve(fc);
+
+  fclone.then((city) => {
+    expect(city).toBe('nyc');
+    done();
+  });
+});
+
+test('ensure async success handler evaluation', done => {
+  const op = new Operation();
+  op.resolve('nyc');
+  op.then((city) => {
+    expect(city).toBe('nyc');
+    doneAlias();
+  });
+
+  const doneAlias = done;
+});
+
+test('ensure async error handler evaluation', done => {
+  const op = new Operation();
+  op.fail(new Error('error: sync fail'));
+  op.catch((err) => {
+    expect(err.message).toBe('error: sync fail');
+    doneAlias();
+  });
+
+  const doneAlias = done;
+});
+
+test('protect from mutiple fail calls', done => {
+  fetchCurrentCityRepeatFail()
+    .catch(e => done());
+});
+
+test('protect from mutiple success calls', done => {
+  fetchCurrentCityIndecisive()
+    .then(c => done());
+});
+
+test('thrown error recovery in catch', done => {
+  fetchCurrentCity()
+    .then((city) => {
+      throw new Error ('then throwes');
+      return fetchWeather(city);
+    })
+    .catch((err) => {
+      expect(err.message).toBe('then throwes');
+      throw new Error('catch throwes');
+    })
+    .catch((err2) => {
+      expect(err2.message).toBe('catch throwes');
+      done();
+    });
+})
+
+test('thrown error recovery', done => {
+  fetchCurrentCity()
+    .then((city) => {
+      throw new Error('oops');
+      return fetchWeather(city);
+    })
+    .catch(e => done());
+});
+
+test('sync result fall through', done => {
+  fetchCurrentCity()
+    .then((city) => {
+      return '10019';
+    })
+    .then((zip) => {
+      expect(zip).toBe('10019');
+      done();
+    });
+});
+
+test('error fallthrough', done => {
+  fetchCurrentCityThatFails()
+    .then((city) => console.log(city))
+    .then((city) => fetchForecast(city))
+    .catch(err => {
+      expect(err).toBe(cityFailError);
+      done();
+    });
+});
+
+test("error recovery async", done => {
+  fetchCurrentCityThatFails()
+    .catch(() => {
+      return fetchCurrentCity();
+      console.log('here!!');
+    })
+    .then((city) => {
+      expect(city).toBe(expectedCurrentCity);
+      done();
+    });
+});
+
+test('error recover', done => {
+  fetchCurrentCityThatFails()
+    .catch((err) => {
+      console.error(err);
+      return 'default city';
+    })
+    .then((city) => {
+      expect(city).toBe('default city');
+      done();
+    });
+});
+
+test('error handler bypass when no error', done => {
+  fetchCurrentCity()
+    .catch(err => "default city")
+    .then(city => {
+      expect(city).toBe(expectedCurrentCity);
+      done();
+    });
+});
 
 test('avoiding nested async', done => {
   let weatherOp = fetchCurrentCity()
